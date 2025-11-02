356c18d74435937899bfedb21128abdad9f390ad
jrw685-design <jrw685@nau.edu>
2025-11-02 04:38:12 -0700

    added note template, updated getNoteId(), added minor css style

--- main/notesScript.js
+++ main/notesScript.js
@@ -1,4 +1,3 @@
-
 let notes = [
     { // start note
         title: "template title",
@@ -20,6 +19,7 @@ function getCurrentDate()
 // each note will have its own html id,
 // allowing them to be referenced easily
 // (e.g. notes[0].title would have an id of "note-0-title" and accessed with getNoteId(0, "title") )
+// types: div (used for entire note div), title, desc, tags, date, edit
 function getNoteId(index, type)
 {
     return "note-" + index + "-" + type;
