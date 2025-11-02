67a9945abf5eeacf5b7b39fc0c2b2bf6f18b512f
jrw685-design <jrw685@nau.edu>
2025-11-02 04:16:15 -0700

    added minor functions/base html

---
+++ main/notesScript.js
@@ -0,0 +1,27 @@
+
+let notes = [
+    { // start note
+        title: "template title",
+        desc: "note content here",
+        date: getCurrentDate(),
+        state: "regular",
+        tags: ["tag1", "tag2"]
+    } // end note
+];
+
+// returns the current date
+function getCurrentDate()
+{
+    let currDate = new Date();
+    console.log(currDate);
+    return currDate;
+    
+}
+
+// each note will have its own html id,
+// allowing them to be referenced easily
+// (e.g. notes[0].title would have an id of "note-0-title" and accessed with getNoteId(0, "title") )
+function getNoteId(index, type)
+{
+    return "note-" + index + "-" + type;
+}
