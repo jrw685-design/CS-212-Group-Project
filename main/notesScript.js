fd7b6389c30ed982db779b1715343479ce0db4ea
jrw685-design <jrw685@nau.edu>
2025-11-06 12:14:11 -0700

    -Changed example note to match javascript -also added jquery

--- main/notesScript.js
+++ main/notesScript.js
@@ -7,7 +7,10 @@ let notes = [
         tags: ["tag1", "tag2"]
     } // end note
 ];
-// test note
+
+// used to add to the array
+let newestIndex = 0;
+
 // returns the current date
 function getCurrentDate()
 {
@@ -24,3 +27,59 @@ function getNoteId(index, type)
 {
     return "note-" + index + "-" + type;
 }
+
+// generates a custom html element
+// just so you dont have to keep typing the really complicated string for it
+// e.g. assuming the note has an index of 0 and has a description of "take out trash at 6:00",
+
+//         getNoteHtml(p, 0, desc, notes[0].desc)
+
+// would return the following:
+
+//         <p class="note-desc" id="note-0-desc">take out trash at 6:00</p>
+
+// IMPORTANT NOTE (pun intended) does not work for div element
+function getNoteHtml(element, index, type, content)
+{
+    return "<" + element + "class=\"note-" + type + "\" id=\"" + getNoteId(index, type) + "\">" + content + "</" + element + ">";
+}
+
+function getTags(tags)
+{
+    let tagsStr = ""
+    for (var i = 0; i < tags.length; i++)
+    {
+        tagsStr += tags[i];
+        if (i != tags.length - 1)
+        {
+            tagsStr += ", ";
+        }
+    }
+    return tagsStr;
+
+}
+
+function addNote()
+{
+    notes[newestIndex].title = "New Note";
+    notes[newestIndex].desc = "";
+    notes[newestIndex].date = getCurrentDate();
+    notes[newestIndex].state = "regular";
+    notes[newestIndex].tags = [];
+
+    // add note to html document with a single string
+    let newNote = "div id=\"" + getNoteId(newestIndex, "div") + "\">" 
+        + getNoteHtml("h2", newestIndex, "title", notes[newestIndex].title) 
+        + getNoteHtml("p", newestIndex, "desc", notes[newestIndex].desc) 
+        + getNoteHtml(getNoteHtml("p", newestIndex, "tags", getTags(notes[newestIndex].tags))) 
+        + getNoteHtml(getNoteHtml("p", newestIndex, "date", notes[newestIndex].date)) 
+        + getNoteHtml(getNoteHtml("button", newestIndex, "editb", "Edit"))
+        + getNoteHtml(getNoteHtml("button", newestIndex, "delb", "Delete"))
+        + "</div>";
+    $("div.all-notes").append(newNote);
+
+    //TODO: add event listeners to edit/delete buttons
+
+    newestIndex++;
+}
+
